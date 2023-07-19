770a18cb46cf73aec4e549fbebb30cdbcfa1df63
Conrad Etherington <conrad.etherington@gmail.com>
2023-07-19 12:53:32 -0600

    first commit, setup tailwind globals

---
+++ app/about-me/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <div className="parent-div" >
+        <div className="text-xl font-semibold">
+            top-level
+        </div>
+        <div>
+            bottom-level
+        </div>
+    </div>
+  )
+}
+
+export default AboutMe
---
+++ components/Whatever.js
@@ -0,0 +1,11 @@
+// "use client";
+// If using hooks
+import React from 'react'
+
+const Whatever = () => {
+  return (
+    <div>Whatever</div>
+  )
+}
+
+export default Whatever
