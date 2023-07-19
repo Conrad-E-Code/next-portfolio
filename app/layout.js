765bb43ebfcc9511ba932883270da8b42cb3557a
Conrad Etherington <conrad.etherington@gmail.com>
2023-07-19 13:13:13 -0600

    used next.js font optimization to self host railway font using next/font/google API

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
  import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import { Raleway } from 'next/font/google'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
+const raleway = Raleway({
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: 'Create Next App',
