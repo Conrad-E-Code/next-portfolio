2637e86f7aee1f3fd58ac807a313275d68cdd5ed
Conrad Etherington <conrad.etherington@gmail.com>
2023-07-19 21:13:49 -0600

    increase z-index of navbar

--- components/Hero.js
+++ components/Hero.js
@@ -4,9 +4,9 @@ const Hero = ({heading, message}) => {
   return (
     <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-cover custom-img'>
         {/* OVERLAY */}
-        <div className='absolute top-10 left-0 right-0 bottom-0 bg-black/40 z-[2]'/>
-        <div className='p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]'>
-            <h2 className='text-5xl font-semibold'>{heading}</h2>
+        <div className='absolute top-10 left-0 right-0 bottom-0 bg-backgroundColor z-[2]'/>
+        <div className='p-5 text-textColorDark z-[2] ml-[-10rem] mt-[-10rem]'>
+            <h2 className='text-5xl font-semibold text-textColorDark'>{heading}</h2>
             <p className= 'py-5 text-xl'>{message}</p>
             <button className="px-8 py-2 border">book</button>
         </div>
--- components/NavBar.js
+++ components/NavBar.js
@@ -6,10 +6,20 @@ import React from 'react'
 const NavBar = () => {
     const router = useRouter()
   return (
-    <div className=' flex flex-col'>
-        <div className='flex'>
+    <div className=' bg-secondaryColor fixed top-0 w-full ease-in z-20'>
+        <div className=' text-textColorLight'>
+            <ul className='flex mx-5 gap-3'>
+                <li>
                 <h1 onClick={()=> router.push("/")} className='cursor-pointer'>Conrad Etherington</h1>
-<Link className='cursor-pointer' href='/projects'> Projects</Link>
+                </li>
+                <li>
+                <Link className='cursor-pointer' href='/projects'> Projects</Link>
+                </li>
+                <li>
+                    <Link href="#skills">Skills</Link>
+                </li>
+            </ul>
+            
         </div>
     </div>
   )
