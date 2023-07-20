e05331f6d157ee1ca9844de28abd68bfd572a519
Conrad Etherington <conrad.etherington@gmail.com>
2023-07-19 22:54:20 -0600

    navbar color change scroll effect

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,20 +7,25 @@ import Colors from '../constants/colors'
 
 const NavBar = () => {
     const [clr, setClr] = useState(Colors.secondaryColor)
+    const [textClr, setTextClr] = useState(Colors.textColorLight)
     const router = useRouter()
     useEffect(()=>{
         const changeColor = () => {
-            if(window.scrollY >= 10){
-                setClr(Colors.primaryColor)
+            if(window.scrollY >= 15){
+                setClr("transparent")
+                setTextClr(Colors.textColorDark)
             }
             else {
-                setClr(Colors.accentColorB)
+                setClr(Colors.secondaryColor)
+                setTextClr(Colors.textColorLight)
             }
         }
+        window.addEventListener("scroll", changeColor)
     },[])
+
   return (
     <div style={{backgroundColor: `${clr}`}} className=' fixed top-0 w-full ease-in z-20'>
-        <div className=' text-textColorLight'>
+        <div style={{color: textClr}}className=''>
             <ul className='flex mx-5 gap-3'>
                 <li className='navbar-li'>
                 <h1 onClick={()=> router.push("/")} className='cursor-pointer'>Conrad Etherington</h1>
