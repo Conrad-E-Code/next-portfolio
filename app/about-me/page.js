9cc795b2e8723be3423063f46327f5f9d382160a
Conrad Etherington <conrad.etherington@gmail.com>
2023-07-26 02:42:37 -0600

    embed resume

--- app/about-me/page.js
+++ app/about-me/page.js
@@ -1,7 +1,6 @@
 "use client";
 import Image from 'next/image';
 import React from 'react'
-import pipeline from "../../constants/images/selim-arda-eryilmaz-XYeCKHcZNz8-unsplash.jpg"
 import { useContext } from 'react';
 import { Context } from '@/context/Context';
 import Colors from '@/constants/colors';
@@ -12,7 +11,9 @@ const AboutMe = () => {
   return (
     <div >
       <Hero message="Glad you're here" heading="CI/CD Pipeline" buttonText={"New Features!"} buttonTextAlt={"Coming Soon."} buttonTarget={"coming-soon"} bg="pipeline"/>
-      <div id="coming-soon" className='h-screen bg-fixed bg-cover small-pipeline'></div>
+      <div id="coming-soon" className='h-screen bg-fixed bg-cover small-pipeline'>
+      <embed src="/resume.pdf" type='application/pdf' width='100%' height='900px'></embed>
+      </div>
     </div>
   )
 }
