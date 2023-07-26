949881ce3134b5fb1fad3fa5cf706f94dd315a47
Conrad Etherington <conrad.etherington@gmail.com>
2023-07-26 03:14:56 -0600

    disable media query for MyVideo Component

--- components/video/MyVideo.js
+++ components/video/MyVideo.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function MyVideo({videoUrl, }){
 
   return (
-    <div className='max-w-[500px] hidden sm:block'>
+    <div className='max-w-[500px]'>
       <iframe mute={1} width="460" height="315" src={videoUrl} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
