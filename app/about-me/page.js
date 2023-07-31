c6066bbc7b46d422ae1db3aaf0135ea2d70ebc11
Conrad Etherington <conrad.etherington@gmail.com>
2023-07-31 14:20:33 -0600

    added serverless routes, install mongobd, mongoose, bcrypt, sign-up route functional

--- app/about-me/page.js
+++ app/about-me/page.js
@@ -1,21 +1,38 @@
 "use client";
-import Image from 'next/image';
 import React from 'react'
 import { useContext } from 'react';
 import { Context } from '@/context/Context';
 import Colors from '@/constants/colors';
 import Hero from '@/components/Hero';
-const AboutMe = () => {
+import { useRouter } from 'next/router';
 
-  const { textClr } = useContext(Context);
+const page = () => {
+    const { nav, setNav, clr, setClr, textClr, setTextClr, setTextIsDark, textIsDark } = useContext(Context)
+
+    const header = `text-4xl font-bold text-${Colors[textClr]}`
+    const paragraph = "text-xl font-semibold"
+
+    console.log(header)
   return (
-    <div >
-      <Hero message="Glad you're here" heading="CI/CD Pipeline" buttonText={"New Features!"} buttonTextAlt={"Coming Soon."} buttonTarget={"coming-soon"} bg="pipeline"/>
-      <div id="coming-soon" className='h-screen bg-fixed bg-cover small-pipeline'>
-      <embed src="/resume.pdf" type='application/pdf' width='100%' height='900px'></embed>
-      </div>
+    <div className='pt-16'>
+        <div className='flex flex-col items-center justify-center'>
+            <h1 className={`${header}`}>About Me</h1>
+            <p className='text-xl font-semibold'>I am a full-stack web developer with a passion for creating beautiful, responsive, and accessible websites and applications.</p>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+            <h1 className={`${header}`}>My Story</h1>
+            <p className={`${paragraph}`}>I am a full-stack web developer with a passion for creating beautiful, responsive, and accessible websites and applications.</p>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+            <h1 className={`${header}`}>My Values</h1>
+            <p className={`${paragraph}`}>I am a full-stack web developer with a passion for creating beautiful, responsive, and accessible websites and applications.</p>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+            <h1 className={`${header}`}>My Interests</h1>
+            <p className={`${paragraph}`}>I am a full-stack web developer with a passion for creating beautiful, responsive, and accessible websites and applications.</p>
+        </div>
     </div>
   )
 }
 
-export default AboutMe
+export default page
---
+++ app/api/sign-in/route.js
@@ -0,0 +1,39 @@
+import { connectToDB } from "@/utils/database";
+import User from "@/models/user";
+import bcrypt, { hash } from "bcrypt";
+
+export const POST = async (req, res) => {
+  console.log("MY REQUEST");
+  console.log(
+    "**************************************************************************************"
+  );
+  const { username, password } = await req.json();
+  try {
+    console.log(username, password);
+    await connectToDB();
+    const user = await User.findOne({ username });
+    if (!user) {
+      console.log("NO USER FOUND");
+        const pHash = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        username,
+        password: pHash, 
+      });
+        await newUser.save();
+        return new Response(JSON.stringify(newUser), { status: 201 });
+    }
+    console.log(user);
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      return new Response(JSON.stringify(user), { status: 200 });
+    } else {
+      return new Response(
+        JSON.stringify({ message: "Password does not match" }),
+        { status: 403 }
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+};
