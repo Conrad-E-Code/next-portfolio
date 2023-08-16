d087622c5a984a874e6cf4990d7375c6b41254e9
Conrad Etherington <conrad.etherington@gmail.com>
2023-08-15 22:33:37 -0600

    fix transition effect issue for hero component

--- components/Hero.js
+++ components/Hero.js
@@ -9,27 +9,21 @@ import Colors from "/constants/colors";
 
 const Hero = ({ heading, message, buttonText, buttonTextAlt, buttonTarget, bg, smbg}) => {
   const { textClr } = useContext(Context);
-  useEffect(()=>{
-    function getUser(){
-    }
-    getUser();
-  },[])
   function handleClick() {
     console.log(buttonTarget)
     const getTarget = document.getElementById(buttonTarget);
     if (getTarget) {getTarget.scrollIntoView({ behavior: "smooth" });
   }
   }
-  // console.log("hero textClr: ", textClr);
   return (
-    <div className={`${bg} sm:${smbg} flex items-center justify-center h-screen bg-fixed bg-cover `}>
+    <div className={`${bg} sm:${smbg} flex items-center justify-center h-screen bg-fixed bg-cover`}>
       {/* OVERLAY */}
 
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-primaryColor/70 z-[2]" />
       <div style={{color: Colors[textClr]}}  className=" text-textColorDark z-[2] ease-in duration-300">
         <h2 className={`text-5xl font-semibold `}>{heading}</h2>
         <p className="py-5 text-xl">{message}</p>
-        <button
+        <div
           onClick={handleClick}
           className={`${
             textClr === "textColorLight"
@@ -41,10 +35,10 @@ const Hero = ({ heading, message, buttonText, buttonTextAlt, buttonTarget, bg, s
               : "border-textColorDark"
           } ${
             textClr === "textColorLight" ? "bg-secondaryColor/50" : ""
-          } transition ease-in-out duration-200`}
+          }cursor-pointer`}
         >
           {textClr === "textColorLight" ? buttonText : buttonTextAlt}
-        </button>
+        </div>
       </div>
     </div>
   );
