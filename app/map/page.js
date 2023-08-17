2c9db4b5c0ca46e3de72d065b9a28dc561850f66
Conrad Etherington <conrad.etherington@gmail.com>
2023-08-16 23:22:13 -0600

    create MapComponent

--- app/map/page.js
+++ app/map/page.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import MapComponent from '../../components/MapComponent';
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
 
 const page = () => {
     //render a map for the entire screen.
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "YOUR_API_KEY"
+      })
   return (
-    <div>page</div>
+    <MapComponent />
   )
 }
 
