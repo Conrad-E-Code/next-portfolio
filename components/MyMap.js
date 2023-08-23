c38c0c147d50d62607894334a8b8dec966879646
Conrad Etherington <conrad.etherington@gmail.com>
2023-08-23 02:52:18 -0600

    added UI features for leaflet demo

--- components/MyMap.js
+++ components/MyMap.js
@@ -3,19 +3,20 @@ import React, { useState } from 'react'
 import countries from "../data/countries.json"
 import { MapContainer, TileLayer, GeoJSON} from 'react-leaflet'
 const MyMap = () => {
-    const [pickedColor, setPickedColor] = useState("#ffff00")
-
-
+    const [pickedColor, setPickedColor] = useState("#8a20bc")
+    const [pickedBorder, setPickedBorder] = useState("#8a20bc")
+  const [pickedThickness, setPickedThickness] = useState("2")
 const countryStyle = {
     fillOpacity: 0.5,
-    color: "black",
-    weight: 2
+    color: pickedBorder,
+    weight: pickedThickness
 }
 
-const colors = ["blue", "black", "green", "purple", "yellow"]
+const colors = ["blue", "red", "green", "purple", "yellow"]
 function changeCountryColor(event) {
     event.target.setStyle({
-        fillColor: document.querySelector("#color-picker").value
+        fillColor: document.querySelector("#color-picker").value,
+        fillOpacity: 0.6
     })
 }
 
@@ -27,7 +28,7 @@ function onEachCountry(feature, layer) {
     layer.options.fillColor = colors[Math.floor(Math.random() * 5)]
     layer.bindPopup(cntryName)
     layer.on({
-        click: 
+        mouseover: 
             changeCountryColor
         // mouseout: (e) => {
         //     setTimeout(()=>{
@@ -51,18 +52,34 @@ function onEachCountry(feature, layer) {
 }
 
   return (
+  <>
     <div id="map">
-    <MapContainer center={[47.608013, -122.335167]} zoom={3} scrollWheelZoom={false}>
+    <MapContainer center={[47.608013, -122.335167]} zoom={1} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
   <GeoJSON style={countryStyle} data={countries} onEachFeature={onEachCountry}/>
 </MapContainer>
+</div>
+<div className='flex'>
+<div className='flex ring p-1 m-4 w-1/4 justify-evenly'> <p>Paintbrush Color:</p>
 <input id="color-picker" type='color' value={pickedColor} onChange={(e) => { e.preventDefault() 
 setPickedColor(e.target.value)
 console.log(pickedColor)}} />
 </div>
+<div className='flex ring p-1 m-4 w-1/4 justify-evenly'> <p>Border Color:</p>
+<input id="color-picker" type='color' value={pickedBorder} onChange={(e) => { e.preventDefault() 
+setPickedBorder(e.target.value)
+console.log(pickedColor)}} />
+</div>
+<div className='flex ring p-1 m-4 w-1/4 justify-evenly'> <p>Border Thickness:</p>
+<input id="color-picker" type='range' min={0} max={4} step={0.25}  value={pickedThickness} onChange={(e) => { e.preventDefault() 
+setPickedThickness(e.target.value)
+console.log(pickedThickness)}} />
+</div>
+</div>
+</>
   )
 }
 
