e8ba3f9797e6616b3d7c4e142ce38222c277c2d4
Conrad Etherington <conrad.etherington@gmail.com>
2023-08-27 18:27:32 -0600

    extracted main section to MainPage componnent

---
+++ components/blog/MainPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div className="relative flex h-full max-w-full flex-1 overflow-hidden bg-gray-800">
+    <div className="flex h-full max-w-full flex-1 flex-col" >
+        <main className="relative h-full w-full transition-width overflow-auto flex-1">
+            <div className="flex h-full">
+                <div className="flex-1 overflow-hidden">
+                    <div className="h-full dark:bg-gray-800">
+                        {/* SKIP 2 DIVS */}
+                        <div className="flex h-full flex-col items-center justify-between pb-64">
+                            {/* Header */}
+                            <div className="align-center flex h-full w-full flex-col justify-center self-center px-2 pb-2 md:pb-[8vh]">
+                                <h1 className="text-4xl font-semibold text-center text-gray-200/30 dark:text-gray-600 ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center flex-grow">
+                                    ConRAD
+                                </h1>
+                            </div>
+
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+
+            </div>
+        </main>
+    </div>
+  </div>
+  )
+}
+
+export default MainPage
