a025825f00d9e8a2c7c05b2bcd0b0b48af8fb82d
Conrad Etherington <conrad.etherington@gmail.com>
2023-08-29 14:40:20 -0600

    created API for user inputs to render to blog posts

--- components/blog/BlogMap.js
+++ components/blog/BlogMap.js
@@ -3,11 +3,16 @@ import React from 'react'
 import ChatPost from './ChatPost'
 import { useRef } from 'react';
 import BlogPost from "./BlogPost"
+import LoadingList from './LoadingList';
 
 const BlogMap = () => {
   return (
     <div id="BlogMap" className='w-full text-[#c9cdd3] bg-gray-50/20 px-20'>
-        <BlogPost />
+     
+     {/* WelCOME CHAT */}
+      {/* <ChatPost /> */}
+            {/* //map through blogs here! */}
+        <LoadingList items={[<BlogPost userInput={"Here is My Text ```console.log('and some code')``` And More Text!"} />]} waitTimer={7000}/>
     </div>
   )
 }
