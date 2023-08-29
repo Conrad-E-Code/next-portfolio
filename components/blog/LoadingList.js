5cd9f11428accd4b239bf1e5057da8323ca1bac6
Conrad Etherington <conrad.etherington@gmail.com>
2023-08-29 16:26:15 -0600

    created BlogListItem component, configure client routing

--- components/blog/LoadingList.js
+++ components/blog/LoadingList.js
@@ -18,7 +18,7 @@ const LoadingList = ({ items, waitTimer }) => {
   }, []);
 
   return (
-    <div className="loading-list">
+    <div className="loading-list" id={`t`}>
       {items.slice(0, loadedItems).map((item, index) => (
         <div key={index} className="loading-list-item">
       {item}
